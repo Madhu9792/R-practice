3600adbdb06b91ddfc60b2a2900ca1d105cf716b
Madhu9792 <thakshila97maduwanthi@gmail.com>
2025-06-03 22:11:02 +0530

    My first commit.

---
+++ practoce1.R
@@ -0,0 +1,10 @@
+# Simple R Program to Calculate the Average
+
+# Create a vector of numbers
+numbers <- c(10, 20, 30, 40, 50)
+
+# Calculate the average
+average <- mean(numbers)
+
+# Print the result
+print(paste("The average is:", average))
